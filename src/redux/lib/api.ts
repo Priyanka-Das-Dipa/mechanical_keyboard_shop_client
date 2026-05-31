a3d27bdb64de05e71b8de6f7d0c5462e2324dc0c
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2026-05-31 16:36:27 +0600

    Fixed connection with backend

--- src/redux/lib/api.ts
+++ src/redux/lib/api.ts
@@ -1,5 +1,5 @@
-export const BASE_URL =process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api/v1';
-
+export const BASE_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api/v1";
 
 export const apiConfig = {
   baseUrl: BASE_URL,
